71c9acc79668d6975bc3db509c775988417eb7bb
Dmitry Filatov <dfilatov@yandex-team.ru>
2012-02-26 16:32:24 +0300

    doc fixed

--- src/jquery.identify/jquery.identify.js
+++ src/jquery.identify/jquery.identify.js
@@ -1,28 +1,32 @@
 /**
  * Identify plugin
  *
+ * Copyright (c) 2009 Filatov Dmitry (dfilatov@yandex-team.ru)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
  * @version 1.0.0
  */
 
 (function($) {
 
-var counter = 0,
+var cnt = 0,
     expando = '__' + (+new Date),
     get = function() {
-        return 'uniq' + ++counter;
+        return 'uniq' + ++cnt;
     };
 
 /**
- * Уникализатор
- * @param {Object} [obj] объект, который нужно идентифицировать, в случае его отсутствия возвращается просто уникальная строка
- * @param {Boolean} [onlyGet=false] возвращать уникальное значение, только если оно уже до этого было присвоено
- * @returns {String} идентификатор
+ * @param {Object} [obj] object to identify, return next unique id if no passed
+ * @param {Boolean} [onlyGet=false] return unique id only if before assigned
+ * @returns {String} unique id
  */
 $.identify = function(obj, onlyGet) {
 
     if(!obj) return get();
 
-    var key = 'uniqueID' in obj? 'uniqueID' : expando; // используем, по возможности. нативный uniqueID для элементов в IE
+    var key = 'uniqueID' in obj? 'uniqueID' : expando; // use native uniqueID for IE elements
 
     return onlyGet || key in obj?
         obj[key] :
